683cd5bd954118a545dca7174f79cdd2380bb4e4
Mazdak Mansouri <mazdak78@yahoo.com>
2019-11-07 15:01:38 -0500

    Create sample-component.vue

---
+++ sample-component.vue
@@ -0,0 +1,20 @@
+  
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "googleAnalytic",
+        methods:{
+            OurMethodWhichConversionHappen(){
+                //our logic
+                
+                gtag_report_conversion_1('/');
+                
+            }
+    }
+</script>
+
+<style scoped>
+</style>
